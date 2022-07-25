ea7a29eec7b23449124df8cce73ea3e400ec8a5c
Anuj Rao <anujabo186@gmail.com>
2022-07-25 18:55:37 +0530

    add learning tests for isNil util

---
+++ src/__tests__/isNil.test.js
@@ -0,0 +1,21 @@
+import _isNil from '../utils/isNil'
+
+describe('_isNil()', () => {
+  test('returns true when passed value is null or undefined', () => {
+    expect(_isNil(null)).toBeTruthy()
+    expect(_isNil(undefined)).toBeTruthy()
+    expect(_isNil()).toBeTruthy()
+  })
+
+  test('returns false when non nulish value is passed', () => {
+    expect(_isNil('qwerty')).toBeFalsy()
+    expect(_isNil(1)).toBeFalsy()
+    expect(_isNil(0)).toBeFalsy()
+    expect(_isNil(false)).toBeFalsy()
+    expect(_isNil(NaN)).toBeFalsy()
+    expect(_isNil({})).toBeFalsy()
+    expect(_isNil([])).toBeFalsy()
+    expect(_isNil(new Set())).toBeFalsy()
+    expect(_isNil(new Map())).toBeFalsy()
+  })
+})
