5f9a2a98eeab90efe03f1d95d985df3938afa168
Anuj Rao <anujabo186@gmail.com>
2022-07-27 18:03:09 +0530

    make test case more descriptive for cloneDeep function related cloning behaviour

--- src/__tests__/cloneDeep.test.js
+++ src/__tests__/cloneDeep.test.js
@@ -1,4 +1,5 @@
 import _cloneDeep from '../utils/cloneDeep'
+import _isFunction from '../utils/isFunction'
 
 describe('_cloneDeep()', () => {
   test('returns null when passed value is null', () => {
@@ -16,11 +17,6 @@ describe('_cloneDeep()', () => {
     expect(_cloneDeep(1.2)).toBe(1.2)
     expect(_cloneDeep(true)).toBeTruthy()
     expect(_cloneDeep(false)).toBeFalsy()
-  })
-
-  test('returns empty object when function is passed', () => {
-    const func = () => { }
-    expect(_cloneDeep(func)).toStrictEqual({})
   })
 
   test('returns a cloned value with each of its property values cloned deeply', () => {
@@ -45,12 +41,19 @@ describe('_cloneDeep()', () => {
     expect(clonedValue.f.h === obj.f.h).toBeFalsy()
   })
 
-  test('returns same function as in original object without cloning', () => {
-    // can we clone a function at all in js. i don't think so ??
+  test('returns empty object when function is passed', () => {
+    const func = () => { }
+    expect(_cloneDeep(func)).toStrictEqual({})
+  })
+
+  test('doesnt clone function into empty object when function is a value of object key or an element of array', () => {
     const obj = {
       func: () => { }
     }
     expect(_cloneDeep(obj).func === obj.func).toBeTruthy()
-  })
 
+    const arr = [() => { }]
+    expect(_isFunction(_cloneDeep(arr)[0])).toBeTruthy()
+    expect(_cloneDeep(arr)[0] === arr[0]).toBeTruthy()
+  })
 })
