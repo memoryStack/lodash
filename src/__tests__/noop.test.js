0bebc6426850c03cc6c4f33f806e93a8c2b37834
Anuj Rao <anujabo186@gmail.com>
2022-08-02 15:01:57 +0530

    remove obvious test case

--- src/__tests__/noop.test.js
+++ src/__tests__/noop.test.js
@@ -2,10 +2,6 @@ import _noop from '../utils/noop'
 import _isFunction from '../utils/isFunction'
 
 describe('_noop()', () => {
-  test('_noop is a function', () => {
-    expect(_isFunction(_noop)).toBeTruthy()
-  })
-
   test('returns undefined always no matter what we pass', () => {
     expect(_noop()).toBeUndefined()
     expect(_noop(1)).toBeUndefined()
