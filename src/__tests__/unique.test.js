eac5602ec06e9268ede7a66ffadb290460c18ec0
Anuj Rao <anujabo186@gmail.com>
2023-07-15 19:24:23 +0530

    Add some learning test cases for unique util

--- src/__tests__/unique.test.js
+++ src/__tests__/unique.test.js
@@ -1,1 +1,18 @@
-// TODO: add test-cases
+import _unique from '../utils/unique'
+
+describe('_unique()', () => {
+  test('will pick first unique entry and remove repeated occurences', () => {
+    expect(_unique([1, 2, 1, 4, 5, 4])).toEqual([1, 2, 4, 5])
+  })
+
+  test('works only for literal values by default', () => {
+    const array = [{ a: 2 }, { a: 1 }, { a: 2 }]
+    expect(_unique(array)).toEqual(array)
+  })
+
+  test('for objects equality comparison we can pass property name', () => {
+    const input = [{ a: 2 }, { a: 1 }, { a: 2 }]
+    const expectedResult = [{ a: 2 }, { a: 1 }, { a: 2 }]
+    expect(_unique(input, 'a')).toEqual(expectedResult)
+  })
+})
