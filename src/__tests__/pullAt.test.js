361f5ddf550e8000690114221645820962ab1243
Anuj Rao <anujabo186@gmail.com>
2023-07-15 20:35:25 +0530

    Add util to extract values at given indexes of array

---
+++ src/__tests__/pullAt.test.js
@@ -0,0 +1,9 @@
+import _pullAt from '../utils/pullAt'
+
+describe('_pullAt()', () => {
+  test('will pick entries from array at given indexes', () => {
+    const array = [1, 2, 1, 4, 5, 4]
+    const indexes = [0, 4]
+    expect(_pullAt(array, indexes)).toEqual([1, 5])
+  })
+})
--- src/__tests__/unique.test.js
+++ src/__tests__/unique.test.js
@@ -1,8 +1,13 @@
 import _unique from '../utils/unique'
 
+// TODO: write test cases for bad inputs as well
 describe('_unique()', () => {
   test('will pick first unique entry and remove repeated occurences', () => {
     expect(_unique([1, 2, 1, 4, 5, 4])).toEqual([1, 2, 4, 5])
+  })
+
+  test('works same way for string iterals as well', () => {
+    expect(_unique(['1', '2', '1', '4', '5', '4'])).toEqual(['1', '2', '4', '5'])
   })
 
   test('works only for literal values by default, will return objects as they are', () => {
